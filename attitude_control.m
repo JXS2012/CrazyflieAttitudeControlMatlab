279ed9621782d9cccd5c7037c657cedeb50ca8fb
JXS2012 <sunjianxin1989@gmail.com>
2015-12-05 12:42:11 -0500

    Update attitude_control.m
    Add comments to inputs

--- attitude_control.m
+++ attitude_control.m
@@ -1,5 +1,13 @@
 function [ u ] = attitude_control( E, e, R, w, d, ddot, M )
 %ATTITUDE_CONTROL attitude controller
+% E 3X3 error in rotation
+% e 3X1 error in angular velocities
+% R 3X3 current rotation Matrix
+% w 3X1 angular velocity
+% d 3X1 desired angular velocity
+% ddot 3X1 desired angular velocity
+% M 3X3 inertia matrix
+
     global constants
 
     u_1 = get_non_gradient_control(E,e,w,d,ddot,M);
